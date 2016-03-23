589aa75df3979a73c1ffaeab021e573c3e9b7985
jaredmichaelsmith <jared@jaredsmith.io>
2016-03-23 12:47:49 -0400

    Move format_keywords function to utils.py

--- loki/utils.py
+++ loki/utils.py
@@ -13,6 +13,14 @@ __maintainer__ = 'Jared M Smith'
 __email__ = 'jared@jaredsmith.io'
 
 
+def format_keywords(keywords):
+    fmt_keywords = []
+    for term_breakdown in keywords:
+        kwd = "".join(term_breakdown)
+        fmt_keywords.append(kwd)
+    return ",".join(fmt_keywords)
+
+
 def log_if_exists(logger, message, level):
     if logger is not None:
         logger.log(message, level)
